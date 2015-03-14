61b0747a1996b47c4f17963477183504693e939f
Nightpaws <nightpaws@users.noreply.github.com>
2015-03-14 21:34:35 +0000

    Error Free Starting Block
    Removed error prone code, and fixed NetBeans warnings

--- JS/fucking-IE.js
+++ JS/fucking-IE.js
@@ -33,41 +33,41 @@ fckIE = {
     body: document.getElementsByTagName("body")[0],
     init: function() {
         var e = -1;
-        if (navigator.appName == "Microsoft Internet Explorer") {
+        if (navigator.appName === "Microsoft Internet Explorer") {
             var t = navigator.userAgent;
             var n = new RegExp("MSIE ([0-9]{1,}[.0-9]{0,})");
-            if (n.exec(t) != null) e = parseFloat(RegExp.$1)
+            if (n.exec(t) !== null) e = parseFloat(RegExp.$1);
         }
-        if (e > fckIE.target.version || e == -1) {
-            return false
+        if (e > fckIE.target.version || e === -1) {
+            return false;
         }
         var r = '<div style="font-size:' + fckIE.font.text + "px; line-height:" + fckIE.font.text + 'px;"><span style="color:#' + fckIE.color.line1 + '">' + fckIE.text.line1 + '</span><br/><span style="color:#' + fckIE.color.line2 + '">' + fckIE.text.line2 + '</span><br/><span style="color:#' + fckIE.color.line3 + '">' + fckIE.text.line3 + "</span></div>",
             i;
         if (fckIE.browser.chrome || fckIE.browser.firefox || fckIE.browser.opera) {
             i = "Download ";
             if (fckIE.browser.chrome) {
-                i = i + '<a style="color:#' + fckIE.color.links + '" href="' + fckIE.link.chrome + '">Chrome</a>'
+                i = i + '<a style="color:#' + fckIE.color.links + '" href="' + fckIE.link.chrome + '">Chrome</a>';
             }
             if (fckIE.browser.firefox) {
                 if (fckIE.browser.chrome) {
-                    i = fckIE.browser.opera ? i + ", " : i + " or "
+                    i = fckIE.browser.opera ? i + ", " : i + " or ";
                 }
-                i = i + '<a style="color:#' + fckIE.color.links + '" href="' + fckIE.link.firefox + '">Firefox</a> '
+                i = i + '<a style="color:#' + fckIE.color.links + '" href="' + fckIE.link.firefox + '">Firefox</a> ';
             }
             if (fckIE.browser.opera) {
                 if (fckIE.browser.chrome || fckIE.browser.firefox) {
-                    i = i + " or "
+                    i = i + " or ";
                 }
-                i = i + '<a style="color:#' + fckIE.color.links + '" href="' + fckIE.link.opera + '">Opera</a> '
+                i = i + '<a style="color:#' + fckIE.color.links + '" href="' + fckIE.link.opera + '">Opera</a> ';
             }
         } else {
-            i = ""
+            i = "";
         }
         var s = '<p style="font-size:' + fckIE.font.download + "px; line-height:" + fckIE.font.download + "px; color:#" + fckIE.color.download + '">' + i + "</p>";
         fckIE.body.style.cssText = "background:#" + fckIE.color.bg;
-        fckIE.body.innerHTML = '<div style="text-align:center; width:70%; margin-left:15%; margin-top:10%; font-family:Helvetica, Arial; font-weight:300">' + r + s + "</div>"
+        fckIE.body.innerHTML = '<div style="text-align:center; width:70%; margin-left:15%; margin-top:10%; font-family:Helvetica, Arial; font-weight:300">' + r + s + "</div>";
     }
 };
 if (window.addEventListener) window.addEventListener("load", fckIE.init(), false);
 else if (window.attachEvent) window.attachEvent("onload", fckIE.init());
-else window.onload = fckIE.init()
+else window.onload = fckIE.init();
