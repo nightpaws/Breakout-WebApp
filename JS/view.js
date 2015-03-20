a31d1b79e8dedd468429734c055ef874159f1f92
Nightpaws <nightpaws@hotmail.co.uk>
2015-03-20 20:48:31 +0000

    Current View
    Realised I've not committed an update to this in a while. Added drawing
    for each type of object onto the canvas

--- JS/view.js
+++ JS/view.js
@@ -1,67 +1,82 @@
 /*jslint node: true, browser: true */
 "use strict";
 
-/* Without other stuff I need the model or MVC won't work) JavaScript is crap*/
 function View(model) {
 
-    var gamediv = document.getElementById("game"); //surrounding game div
-    var gameCanvas = document.getElementById("gameCanvas"); //link html canvas
+    var gameCanvas = document.getElementById("gameCanvas")
+    var gameDiv = document.getElementById("game");
     var firstRun = true; //To allow 'constructor' like call on first run
-    var canvas = canvasElement.getContext("2d");
+    var canvas = gameCanvas.getContext("2d");
+
 
     /*This function will be used to update the positioning in the view, like in GizmoBall*/
     this.update = function() {
-        console.log("Model: update called!")
+        // console.log("View: update called!");
 
+        /*Set canvas dimensions*/
         if (firstRun) {
-            gameCanvas.style.height = gamediv.clientHeight + "px";
-            gameCanvas.style.width = gamediv.clientWidth + "px";
-            canvas.canvas.height = gamediv.clientHeight;
-            canvas.canvas.width = gamediv.clientWidth;
-
+            gameCanvas.style.height = gameDiv.clientHeight + "px";
+            gameCanvas.style.width = gameDiv.clientWidth + "px";
+            canvas.canvas.height = gameDiv.clientHeight;
+            canvas.canvas.width = gameDiv.clientWidth;
             firstRun = false;
         }
+        //Clear before we start
+        this.clearCanvas();
 
-        this.clearGame();
-        this.paddleDraw();
-        // this.brickDraw();
-        // this.ballDraw();
-
-
+        //Draw the objects on the canvas
+        this.drawPaddle();
+        this.drawBricks();
+        this.drawBall();
     };
 
+    this.clearCanvas = function() {
+        // console.log("View: clearCanvas called!");
 
-    /*Functions for drawing each type of object */
-    /*this.draw = function() {
-    //Not yet implemented - May not be needed.
-    }*/
-
-    this.paddleDraw = function() {
-        console.log("Model: paddleDraw called!");
-
-        var paddle = model.getPaddleData();
-
-        canvas.fillStyle = "#FFFFFF";
-        canvas.fillRect(paddle.x, paddle.y, paddle.width, paddle.height);
-
-    };
-
-    this.ballDraw = function() {
-        console.log("Model: ballDraw called!");
-
-    };
-
-    this.brickDraw = function() {
-        console.log("Model: brickDraw called!");
-
-    };
-
-    /*Bin everything */
-    this.clearGame = function() {
-        console.log("Model: clear called!");
         canvas.clearRect(0, 0, canvas.canvas.width, canvas.canvas.height);
-
     };
+
+
+    this.drawPaddle = function() {
+        // console.log("View: drawPaddle called!");
+
+        var paddle = model.getPaddleData(); //retrieve model data
+        canvas.fillStyle = "#FFA500";
+        canvas.fillRect(paddle.x, paddle.y, paddle.width, paddle.height);
+    };
+
+    this.drawBricks = function() {
+        // console.log("View: drawBricks called!");
+
+        var bricks = model.getBricks(); //retrieve model data
+
+        for (var x = 0; x < bricks.length; x++) {
+            for (var y = 0; y < bricks[x].length; y++) {
+
+                var brick = bricks[x][y];
+
+                if (brick.getVisible()) {
+                    var color = brick.getColor();
+                    canvas.fillStyle = color;
+                    canvas.fillRect(brick.getX(), brick.getY(),
+                        brick.getWidth(), brick.getHeight());
+                }
+            }
+        }
+    };
+
+    this.drawBall = function() {
+        // console.log("View: drawBall called!");
+
+        var ball = model.getBallData(); //retrieve model data
+        //base of ball
+        canvas.fillStyle = "#00FF00"; //set colour
+        canvas.beginPath();
+        canvas.lineWidth = 0;
+        canvas.arc(ball.x, ball.y, ball.width / 2, 0, 2 * Math.PI);
+        canvas.fill();
+
+    }
 
 
 }
